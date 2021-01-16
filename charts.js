594daeec9f8ee8776b06a74c5fdd00ed7695665d
Carina Brücher <cbr@koempf24.de>
2021-01-16 16:04:01 +0100

    add new vertical line chart

--- charts.js
+++ charts.js
@@ -141,3 +141,83 @@ pieSeries.slices.template.strokeOpacity = 1;
 
 //line charts climate
 //climate line 1
+var chart = am4core.create("climate1", am4charts.XYChart);
+
+// Add data
+chart.data = [{
+    "question": "1",
+    "used": 75,
+    "new": 67,
+}, {
+    "question": "2",
+    "used": 73,
+    "new": 55,
+}, {
+    "question": "3",
+    "used": 66,
+    "new": 28,
+}, {
+    "question": "4",
+    "used": 66,
+    "new": 53,
+}, {
+    "question": "5",
+    "used": 64,
+    "new": 58,
+}, {
+    "question": "6",
+    "used": 57,
+    "new": 35,
+}, {
+    "question": "7",
+    "used": 56,
+    "new": 50,
+}, {
+    "question": "8",
+    "used": 55,
+    "new": 25,
+}, {
+    "question": "9",
+    "used": 16,
+    "new": 27,
+}];
+
+// Create axes
+var dateAxis = chart.yAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.inversed = true;
+
+// Create value axis
+var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+
+// Create series
+var series1 = chart.series.push(new am4charts.LineSeries());
+series1.dataFields.valueX = "used";
+series1.dataFields.dateY = "question";
+series1.strokeWidth = 3;
+series1.strokeDasharray = 4;
+series1.bullets.push(new am4charts.CircleBullet());
+series1.tooltipText = "{valueX.value}";
+
+var series2 = chart.series.push(new am4charts.LineSeries());
+series2.dataFields.valueX = "new";
+series2.dataFields.dateY = "question";
+series2.strokeWidth = 3;
+series2.strokeDasharray = 4;
+series2.bullets.push(new am4charts.CircleBullet());
+series2.tooltipText = "{valueX.value}";
+
+// Add chart cursor
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.behavior = "zoomY";
+
+// Create ranges
+var range = valueAxis.axisRanges.create();
+range.value = 0;
+range.endValue = 100;
+range.strictMinMax= true;
+range.renderer.minGridDistance = 25;
+
+
+var range2 = dateAxis.axisRanges.create();
+range2.value = 120;
+range2.endValue = 200;
